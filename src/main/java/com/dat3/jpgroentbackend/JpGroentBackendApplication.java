4a3c70d99d87a3dc07cc4df556ac8c34c0ad20f1
Kristoffer <XA68OU@student.aau.dk>
2024-10-09 14:00:13 +0200

    Initial commit

---
+++ src/main/java/com/dat3/jpgroentbackend/JpGroentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dat3.jpgroentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpGroentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpGroentBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dat3/jpgroentbackend/JpGroentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dat3.jpgroentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpGroentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
