9bbb6b1650c36b0553b0b0cd8cb9dd271e168178
victorhjelmberg <vhf@nyvej37.dk>
2024-11-01 15:32:01 +0100

    OpenApi setup

---
+++ src/main/java/com/dat3/jpgroentbackend/controller/Controller.java
@@ -0,0 +1,21 @@
+package com.dat3.jpgroentbackend.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+//@ApiResponse(content = { @Content(mediaType = "application/json"));
+@RestController
+public class Controller {
+    /*
+    @GetMapping("/hello/{language}")
+    public SimpleMessage hello(
+        @PathVariable("language") String language
+    ) {
+        return new SimpleMessage("Hello world! Sorry i don't speak " + language);
+    }
+    @PostMapping("/hello")
+    public SimpleMessage postme() {
+        return new SimpleMessage("Hello world!");
+    }
+    */
+}
