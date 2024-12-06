c5b0fa178dcf7b4e3914fe49ad42b1f27dc3afe1
Nicolai Dybro <nicolai160g@gmail.com>
2024-12-06 15:19:09 +0100

    AAUJP-166 Lavet videre på test

--- src/test/java/com/dat3/jpgroentbackend/JpGroentBackendApplicationTests.java
+++ src/test/java/com/dat3/jpgroentbackend/JpGroentBackendApplicationTests.java
@@ -2,6 +2,7 @@ package com.dat3.jpgroentbackend;
 
 import com.dat3.jpgroentbackend.model.*;
 import com.dat3.jpgroentbackend.model.repositories.UserRepository;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,9 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.time.Instant;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -75,15 +74,14 @@ public class JpGroentBackendApplicationTests {
     @Order(1)
     @Test
     void userAuth() throws Exception {
-        String responseBody = mockMvc.perform(post("/api/auth/login")
+
+        authToken = mockMvc.perform(post("/api/auth/login")
                         .contentType("application/json")
                         .content("{\"username\": \"demo\", \"password\": \"12345\"}"))
                 .andExpect(status().isOk())
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
-
-        authToken = responseBody;
 
         // Assume token exists for further tests to run
         assumeTrue(!authToken.isEmpty(), "Auth token not received. Skipping further tests.");
@@ -106,8 +104,117 @@ public class JpGroentBackendApplicationTests {
                 .andExpect(jsonPath("$.active").value(true))
                 .andExpect(jsonPath("$.password").isString());
     }
+    @Order(3)
+    @Test
+    void createRack() throws Exception {
+        assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
+
+        mockMvc.perform(post("/Rack")
+                        .contentType("application/json")
+                        .content("{\"xCoordinate\": 10, \"yCoordinate\": 10}")
+                        .header("Authorization", "Bearer " + authToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.position.y").value(10))
+                .andExpect(jsonPath("$.position.x").value(10))
+                .andExpect(jsonPath("$.shelves").isArray())
+                .andExpect(jsonPath("$.shelves").isEmpty());
+    }
+
+    @Order(4)
+    @Test
+    void createShelf() throws Exception {
+        assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
+
+        String rackId = "1";
+
+        mockMvc.perform(post("/Rack/{rackId}/Shelf", rackId)
+                        .contentType("application/json")
+                        .header("Authorization", "Bearer " + authToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.position.y").value(10))
+                .andExpect(jsonPath("$.position.x").value(10))
+                .andExpect(jsonPath("$.shelves[0].id").value(1))
+                .andExpect(jsonPath("$.shelves[0].batches").isArray())
+                .andExpect(jsonPath("$.shelves[0].batches").isEmpty());
+    }
+
+    @Order(5)
+    @Test
+    void createPlantType() throws Exception {
+        assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
+
+        mockMvc.perform(post("/PlantType")
+                        .contentType("application/json")
+                        .content("""
+                                {
+                                  "name": "Solsikke",
+                                  "preGerminationDays": 1,
+                                  "growthTimeDays": 10,
+                                  "preferredPosition": "Low",
+                                  "wateringSchedule": [
+                                    1,
+                                    5,
+                                    7
+                                  ]
+                                }""")
+                        .header("Authorization", "Bearer " + authToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Solsikke"))
+                .andExpect(jsonPath("$.preGerminationDays").value(1))
+                .andExpect(jsonPath("$.growthTimeDays").value(10))
+                .andExpect(jsonPath("$.preferredPosition").value("Low"))
+                .andExpect(jsonPath("$.wateringSchedule").isArray())
+                .andExpect(jsonPath("$.wateringSchedule[0]").value(1))
+                .andExpect(jsonPath("$.wateringSchedule[1]").value(5))
+                .andExpect(jsonPath("$.wateringSchedule[2]").value(7));
+    }
+
+    @Order(6)
+    @Test
+    void createTrayType() throws Exception {
+        assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
+
+        mockMvc.perform(post("/TrayType")
+                        .contentType("application/json")
+                        .content("""
+                                {
+                                  "name": "Stor",
+                                  "widthCm": 15,
+                                  "lengthCm": 15
+                                }""")
+                        .header("Authorization", "Bearer " + authToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Stor"))
+                .andExpect(jsonPath("$.widthCm").value(15))
+                .andExpect(jsonPath("$.lengthCm").value(15));
+    }
+
+    @Order(7)
+    @Test
+    void createBatch() throws Exception {
+        assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
+
+        mockMvc.perform(post("/Batch")
+                        .contentType("application/json")
+                        .content("""
+                                {
+                                  "plantTypeId": "Solsikke",
+                                  "trayTypeId": "Stor",
+                                  "createdByUsername": "admin",
+                                  "amount": 10
+                                }""")
+                        .header("Authorization", "Bearer " + authToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.plantTypeId").value("Solsikke"))
+                .andExpect(jsonPath("$.trayTypeId").value("Stor"))
+                .andExpect(jsonPath("$.createdAt").value(Matchers.matchesPattern("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.*$")))
+                .andExpect(jsonPath("$.createdBy").value("admin"));
+        }
 
     // ALL GET REQUEST
+    @Order(8)
     @Test
     void getUsers() throws Exception { // done
         assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
@@ -121,41 +228,76 @@ public class JpGroentBackendApplicationTests {
                 .andExpect(jsonPath("$[0].password").isString());
     }
 
+    @Order(9)
     @Test
     void getTask() throws Exception {
         assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
 
     }
 
+    @Order(10)
     @Test
     void getRacks() throws Exception {
         assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
 
     }
 
+    @Order(11)
     @Test
     void getBatchesOnRack() throws Exception {
         assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
     }
 
+    @Order(12)
     @Test
     void getPregerminatingBatches() throws Exception {
         assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
     }
 
+    @Order(13)
     @Test
     void getMaxBatchAmountOnShelf() throws Exception {
         assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
+
+        String batchId = "1";
+
+        mockMvc.perform(get("/Batch/{batchId}/MaxAmountOnShelves", batchId)
+                        .header("Authorization", "Bearer " + authToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value("Stor"))
+                .andExpect(jsonPath("$.1[0]").value(1600)) // Forventer at "1" indeholder 1600 i den første position
+                .andExpect(jsonPath("$.2").isEmpty()); // Forventer at "2" er en tom liste
     }
 
+    @Order(14)
     @Test
     void getTrayTypes() throws Exception {
         assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
+
+        mockMvc.perform(get("/TrayTypes")
+                        .header("Authorization", "Bearer " + authToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("Stor"))
+                .andExpect(jsonPath("$[0].widthCm").value(15))
+                .andExpect(jsonPath("$[0].lengthCm").value(15));
     }
 
+    @Order(15)
     @Test
     void getPlantTypes() throws Exception {
         assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
+
+        mockMvc.perform(get("/PlantTypes")
+                        .header("Authorization", "Bearer " + authToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("Solsikke"))
+                .andExpect(jsonPath("$[0].preGerminationDays").value(1))
+                .andExpect(jsonPath("$[0].growthTimeDays").value(10))
+                .andExpect(jsonPath("$[0].preferredPosition").value("Low"))
+                .andExpect(jsonPath("$[0].wateringSchedule").isArray())
+                .andExpect(jsonPath("$[0].wateringSchedule[0]").value(1))
+                .andExpect(jsonPath("$[0].wateringSchedule[1]").value(5))
+                .andExpect(jsonPath("$[0].wateringSchedule[2]").value(7));
     }
 
 
@@ -215,16 +357,9 @@ public class JpGroentBackendApplicationTests {
 
     }
 
-    @Test
-    void createRack() throws Exception {
-        assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
 
-    }
 
-    @Test
-    void createShelf() throws Exception {
-        assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
-    }
+
 
     @Test
     void deleteShelf() throws Exception {
@@ -243,17 +378,10 @@ public class JpGroentBackendApplicationTests {
         assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
     }
 
-    @Test
-    void createBatch() throws Exception {
-        assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
-    }
+
 
 
     // TRAYTYPE
-    @Test
-    void createTrayType() throws Exception {
-        assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
-    }
 
     @Test
     void deleteTrayType() throws Exception {
@@ -261,10 +389,6 @@ public class JpGroentBackendApplicationTests {
     }
 
     // PLANTTYPE
-    @Test
-    void createPlantType() throws Exception {
-        assumeTrue(authToken != null && !authToken.isEmpty(), "Authentication failed, skipping updateUserDetails test.");
-    }
 
     @Test
     void deletePlantType() throws Exception {
