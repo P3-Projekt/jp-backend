d6422aa06a8a89557b0893c3e0aa9de56b3f4dc7
Jacob Højlund Simonsen <jsimon23@student.aau.dk>
2024-12-08 23:25:11 +0100

    Move methods out of BatchController and into respective classes (Batch, Rack, Shelf)

--- src/main/java/com/dat3/jpgroentbackend/controllers/BatchController.java
+++ src/main/java/com/dat3/jpgroentbackend/controllers/BatchController.java
@@ -8,6 +8,7 @@ import com.dat3.jpgroentbackend.controllers.dto.response.MaxAmountOnShelvesRespo
 import com.dat3.jpgroentbackend.controllers.dto.response.PreGerminatingBatchesResponse;
 import com.dat3.jpgroentbackend.model.*;
 import com.dat3.jpgroentbackend.model.repositories.*;
+import com.dat3.jpgroentbackend.model.repositories.utils.ShelfScore;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -16,9 +17,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-import com.dat3.jpgroentbackend.model.PlantType.PreferredPosition;
-
-import java.time.LocalDate;
 import java.util.*;
 
 
@@ -36,13 +34,13 @@ public class BatchController {
     @Autowired
     private TrayTypeRepository trayTypeRepository;
     @Autowired
-    private BatchLocationRepository batchLocationRepository;
+    private BatchLocationRepository batchLocationRepository; // Do we need this?
     @Autowired
     private ShelfRepository shelfRepository;
     @Autowired
     private RackRepository rackRepository;
     @Autowired
-    private SecurityExpressionHandler securityExpressionHandler;
+    private SecurityExpressionHandler securityExpressionHandler; // Do we need this?
 
     /**
      * Creates a new batch based on the request data.
@@ -162,7 +160,6 @@ public class BatchController {
         int locatedAmount = request.locations.values().stream().reduce(0, Integer::sum);
         if(batchAmount != locatedAmount) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Located amount was " + locatedAmount + " but batch contains " + batchAmount + " fields");
 
-
         // Create batchLocations with the values from the request body
         List<BatchLocation> batchLocations = new ArrayList<>();
         for (Map.Entry<Integer, Integer> location : request.locations.entrySet()) {
@@ -187,32 +184,6 @@ public class BatchController {
         batchRepository.save(batch);
         }
 
-    class ScoreObj {
-        private final int shelfId;
-        private final int score;
-        private final int amount;
-
-        // Constructor
-        private ScoreObj(int shelfId, int score, int amount) {
-            this.shelfId = shelfId;
-            this.score = score;
-            this.amount = amount;
-        }
-
-        // Getters
-        private int getScore() {
-            return score;
-        }
-
-        private int getAmount() {
-            return amount;
-        }
-
-        private int getShelfId() {
-            return shelfId;
-        }
-    }
-
     /**
      * Suggests optimal locations for placing a batch based on predefined criteria.
      * @param batchId ID of the batch
@@ -227,92 +198,32 @@ public class BatchController {
     ) {
         // Check if the batch exists and get a batch object
         Batch batch = batchRepository.findById(batchId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "A batch with id '" + batchId + "' does not exist"));
-        PlantType plantType = batch.getPlantType();
-        PreferredPosition preferredPosition = plantType.getPreferredPosition();
-        
+
         // Throw an exception if the batch has already been placed
         if (batch.getBatchLocations().size() != 1 && batch.getBatchLocations().getFirst().getShelf() != null) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Batch has already been placed");
         }
-        
-        List<ScoreObj> scoreList = new ArrayList<>();
+
+        // Find all racks, save them to a list
         List<Rack> racks = rackRepository.findAll();
 
-        for (Rack rack : racks) {
-            // If no shelves on this rack, continue to the next rack
-            if (rack.getShelves() == null) {
-                continue;
-            }
+        // Get a list of shelfscore objects
+        List<ShelfScore> scoreList = batch.autolocate(racks);
 
-            // Get a list of the maximum amount of this batch that can be on each shelf
-            List<Integer> maxAmountOnShelves = rack.getMaxAmountOnShelves(batch);
-
-            int highShelfIndex = rack.getShelves().size(); // Top position in the rack
-
-            boolean rackContainsBatches = rackContainsBatches(rack); // True if the rack contains any batches at all
-
-            // Go through all shelves in a rack, from the lowest one, as the positions are incremented each time; if we wanted to start from the top reversed() would be necessary
-            for (Shelf shelf : rack.getShelves()) {
-
-                // Get the max amount from this batch that can be placed on the current shelf, must be reversed as maxAmountOnShelves is also reversed
-                int maxOnThisShelf = maxAmountOnShelves.reversed().get(shelf.getPosition() - 1);
-
-                // If the shelf cant have any trays of the batch on it, continue
-                if (maxOnThisShelf == 0) continue;
-
-                // Sets the score to 10 or 0 depending on if the rack contains batches
-                int score = rackContainsBatches ? 10 : 0;
-
-                // Increases score if the plant type has a preferred position and the shelf matches the preferred position
-                if ((shelf.getPosition() == 1 && preferredPosition == PreferredPosition.Low) ||
-                        (shelf.getPosition() == highShelfIndex && preferredPosition == PreferredPosition.High)) {
-                    score += 100;
-                } else if (preferredPosition != PreferredPosition.NoPreferred) { // Otherwise if the preferredPosition is not NoPreferred
-                    continue;
-                }
-
-                // Get all the plant types on the current shelf
-                Set<PlantType> plantTypesOnShelf = getPlantTypesOnShelf(shelf);
-
-                // If the shelf does NOT already contain the plant type which the batch has, increase score
-                if (!plantTypesOnShelf.contains(plantType)) {
-                    score += 50;
-                }
-
-                // Get all the harvest dates of batches on the current shelf
-                Set<LocalDate> harvestDatesOnShelf = getHarvestDatesOnShelf(shelf);
-
-                // If there is any batch with the same harvest date on the current shelf, increase score
-                if (harvestDatesOnShelf.contains(batch.getHarvestingTask().getDueDate())) {
-                    score += 25;
-                }
-
-                scoreList.add(new ScoreObj(shelf.getId(), score, maxOnThisShelf));
-            }
-        }
-
-        // Sort the score list by highest score, descending
-        scoreList.sort(Comparator.comparingInt(ScoreObj::getScore).reversed());
-
-        // The total amount of fields in the batch
-        int amountToBePlaced = batch.getAmount();
-
-        return getBatchesOnRack(scoreList, amountToBePlaced);
-    }
-
-    private Map<Integer, Integer> getBatchesOnRack(List<ScoreObj> scoreList, int amountToBePlaced) {
         // Create a new map to store the data in
         Map<Integer, Integer> batchesOnRacks = new HashMap<>();
 
         // Create an iterator to easily iterate through the list
-        Iterator<ScoreObj> scoreIterator = scoreList.iterator();
+        Iterator<ShelfScore> scoreIterator = scoreList.iterator();
+
+        int amountToBePlaced = batch.getAmount();
 
         // Loop while we still need to place some and while there are still more score objects in the iterator
         while (amountToBePlaced > 0 && scoreIterator.hasNext()) {
 
-            ScoreObj currentScoreObj = scoreIterator.next(); // Get the current score object
-            int shelfId = currentScoreObj.getShelfId();
-            int amount = currentScoreObj.getAmount();
+            ShelfScore currentShelfScore = scoreIterator.next(); // Get the current score object
+            int shelfId = currentShelfScore.getShelfId();
+            int amount = currentShelfScore.getAmount();
 
             // Get the minimum between amountToBePlaced and the amount that can be placed on this one
             int amountToBePlacedOnThisShelf = Math.min(amount, amountToBePlaced);
@@ -324,46 +235,5 @@ public class BatchController {
             amountToBePlaced -= amountToBePlacedOnThisShelf;
         }
         return batchesOnRacks;
-    }
-
-    // Helper methods
-    /**
-     * Get all the harvest dates of batches on a shelf
-     * @param shelf The shelf to get the harvest dates from
-     * @return A set of all the harvest dates of batches on the shelf
-     */
-    private Set<LocalDate> getHarvestDatesOnShelf(Shelf shelf) {
-        Set<LocalDate> harvestDatesOnShelf = new HashSet<>();
-        for (BatchLocation batchLocation : shelf.getBatchLocations()) {
-            harvestDatesOnShelf.add(batchLocation.getBatch().getHarvestingTask().getDueDate());
-        }
-        return harvestDatesOnShelf;
-    }
-
-    /**
-     * Get all the plant types on a shelf
-     * @param shelf The shelf to get the plant types from
-     * @return A set of all the plant types on the shelf
-     */
-    private Set<PlantType> getPlantTypesOnShelf(Shelf shelf) {
-        Set<PlantType> plantTypesOnShelf = new HashSet<>();
-        for (BatchLocation batchLocation : shelf.getBatchLocations()) {
-            plantTypesOnShelf.add(batchLocation.getBatch().getPlantType());
-        }
-        return plantTypesOnShelf;
-    }
-
-    /**
-     * Check if a rack contains any batches
-     * @param rack The rack to check
-     * @return True if the rack contains any batches, false otherwise
-     */
-    private boolean rackContainsBatches(Rack rack) {
-        for (Shelf shelf : rack.getShelves()) {
-            if (!shelf.getBatchLocations().isEmpty()) {
-                return true;
-            }
-        }
-        return false;
     }
 }
